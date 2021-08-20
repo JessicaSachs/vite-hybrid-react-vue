caf799517deb84a2b80ceba8173e6278af944ad4
Jessica Sachs <jess@jessicasachs.io>
2021-08-20 00:08:40 -0400

    Update entry.tsx

--- src/react/entry.tsx
+++ src/react/entry.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-export async function renderReact(target = document.getElementById('root'), props = {}) {
+export async function renderReact(target = document.getElementById('root')) {
   const ReactApp = (await import('./App')).default // Styles only load when the method is called.
 
   return ReactDOM.render(
